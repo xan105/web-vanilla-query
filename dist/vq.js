3cb830b5f973e752ae5c15af95166ad764cd05f8
Anthony Beaumont <dev.xan105@outlook.be>
2022-12-28 14:34:45 +0700

    v1.3.0

--- dist/vq.js
+++ dist/vq.js
@@ -4,64 +4,51 @@ var __export = (target, all) => {
     __defProp(target, name, { get: all[name], enumerable: true });
 };
 
-// lib/param.js
-var param = {
-  configurable: false,
-  enumerable: false,
-  writable: false
-};
-
 // lib/fx.js
 var fx_exports = {};
 __export(fx_exports, {
   $fadeIn: () => $fadeIn,
   $fadeOut: () => $fadeOut
 });
-var $fadeOut = {
-  value: (duration = 400) => {
-    const el = void 0;
-    return new Promise((resolve) => {
-      el.style["pointer-events"] = "none";
-      el.style.opacity = 1;
-      let previous = +new Date();
-      (function fade() {
-        el.style.opacity = +el.style.opacity - (new Date() - previous) / duration;
-        previous = +new Date();
-        if (+el.style.opacity <= 0) {
-          el.style.display = "none";
-          el.style.removeProperty("opacity");
-          el.style["pointer-events"] = "";
-          return resolve(el);
-        } else if (+el.style.opacity > 0)
-          requestAnimationFrame(fade);
-      })();
-    });
-  },
-  ...param
+var $fadeOut = (duration = 400) => {
+  const el = void 0;
+  return new Promise((resolve) => {
+    el.style["pointer-events"] = "none";
+    el.style.opacity = 1;
+    let previous = +new Date();
+    (function fade() {
+      el.style.opacity = +el.style.opacity - (new Date() - previous) / duration;
+      previous = +new Date();
+      if (+el.style.opacity <= 0) {
+        el.style.display = "none";
+        el.style.removeProperty("opacity");
+        el.style["pointer-events"] = "";
+        return resolve(el);
+      } else if (+el.style.opacity > 0)
+        requestAnimationFrame(fade);
+    })();
+  });
 };
-var $fadeIn = {
-  value: (duration = 400) => {
-    const el = void 0;
-    return new Promise((resolve) => {
-      el.style["pointer-events"] = "none";
-      el.style.opacity = 0;
-      el.style.display = "";
-      if (getComputedStyle(el).display === "none")
-        el.style.display = "block";
-      let previous = +new Date();
-      (function fade() {
-        el.style.opacity = +el.style.opacity + (new Date() - previous) / duration;
-        previous = +new Date();
-        if (+el.style.opacity >= 1) {
-          el.style.removeProperty("opacity");
-          el.style["pointer-events"] = "";
-          return resolve(el);
-        } else if (+el.style.opacity < 1)
-          requestAnimationFrame(fade);
-      })();
-    });
-  },
-  ...param
+var $fadeIn = (duration = 400) => {
+  const el = void 0;
+  return new Promise((resolve) => {
+    el.style["pointer-events"] = "none";
+    el.style.opacity = 0;
+    el.style.display = "";
+    if (getComputedStyle(el).display === "none")
+      el.style.display = "block";
+    let previous = +new Date();
+    (function fade() {
+      el.style.opacity = +el.style.opacity + (new Date() - previous) / duration;
+      previous = +new Date();
+      if (+el.style.opacity >= 1) {
+        el.style.removeProperty("opacity");
+        el.style["pointer-events"] = "";
+        return resolve(el);
+      } else if (+el.style.opacity < 1)
+        requestAnimationFrame(fade);
+    })();
+  });
 };
 
 // lib/helper.js
@@ -86,190 +73,136 @@ __export(helper_exports, {
   $text: () => $text,
   $toggleClass: () => $toggleClass
 });
-var $addClass = {
-  value: function(name) {
-    this.classList.add(name);
-    return this;
-  },
-  ...param
+var $addClass = function(name) {
+  this.classList.add(name);
+  return this;
 };
-var $removeClass = {
-  value: function(name) {
-    this.classList.remove(name);
-    return this;
-  },
-  ...param
+var $removeClass = function(name) {
+  this.classList.remove(name);
+  return this;
 };
-var $toggleClass = {
-  value: function(name) {
-    this.classList.toggle(name);
-    return this;
-  },
-  ...param
+var $toggleClass = function(name) {
+  this.classList.toggle(name);
+  return this;
 };
-var $hasClass = {
-  value: function(name) {
-    return this.classList.contains(name);
-  },
-  ...param
+var $hasClass = function(name) {
+  return this.classList.contains(name);
 };
-var $html = {
-  value: function(value = null) {
-    if (value == null)
-      return this.innerHTML;
-    this.innerHTML = value;
-    return this;
-  },
-  ...param
+var $html = function(value = null) {
+  if (value == null)
+    return this.innerHTML;
+  this.innerHTML = value;
+  return this;
 };
-var $css = {
-  value: function(name, value = null) {
-    if (value == null)
-      return this.style[name];
-    this.style[name] = value;
-    return this;
-  },
-  ...param
+var $css = function(name, value = null) {
+  if (value == null)
+    return this.style[name];
+  this.style[name] = value;
+  return this;
 };
-var $text = {
-  value: function(value = null) {
-    if (value == null)
-      return this.textContent;
-    this.textContent = value;
-    return this;
-  },
-  ...param
+var $text = function(value = null) {
+  if (value == null)
+    return this.textContent;
+  this.textContent = value;
+  return this;
 };
-var $attr = {
-  value: function(name, value = null) {
-    if (value == null)
-      return this.getAttribute(name);
-    this.setAttribute(name, value);
-    return this;
-  },
-  ...param
+var $attr = function(name, value = null) {
+  if (value == null)
+    return this.getAttribute(name);
+  this.setAttribute(name, value);
+  return this;
 };
-var $empty = {
-  value: function() {
-    while (this.firstChild)
-      this.removeChild(this.firstChild);
-    return this;
-  },
-  ...param
+var $empty = function() {
+  while (this.firstChild)
+    this.removeChild(this.firstChild);
+  return this;
 };
-var $show = {
-  value: function() {
-    this.style.display = "";
-    if (getComputedStyle(this).display === "none")
-      this.style.display = "block";
-    return this;
-  },
-  ...param
+var $show = function() {
+  this.style.display = "";
+  if (getComputedStyle(this).display === "none")
+    this.style.display = "block";
+  return this;
 };
-var $hide = {
-  value: function() {
-    this.style.display = "none";
-    return this;
-  },
-  ...param
+var $hide = function() {
+  this.style.display = "none";
+  return this;
 };
-var $append = {
-  value: function(html) {
-    this.insertAdjacentHTML("beforeend", html);
-    return this;
-  },
-  ...param
+var $append = function(html) {
+  this.insertAdjacentHTML("beforeend", html);
+  return this;
 };
-var $prepend = {
-  value: function(html) {
-    this.insertAdjacentHTML("afterbegin", html);
-    return this;
-  },
-  ...param
+var $prepend = function(html) {
+  this.insertAdjacentHTML("afterbegin", html);
+  return this;
 };
-var $on = {
-  value: function(event, callback) {
-    this.addEventListener(event, callback, false);
-    if (!this.$__events__)
-      this.$__events__ = /* @__PURE__ */ Object.create(null);
-    if (!this.$__events__[event])
-      this.$__events__[event] = /* @__PURE__ */ new Set();
-    this.$__events__[event].add(callback);
-  },
-  ...param
-};
-var $once = {
-  value: function(event, callback) {
-    this.addEventListener(event, callback, {
-      capture: false,
-      once: true
+var $on = function(event, callback) {
+  this.addEventListener(event, callback, false);
+  if (!this.$__events__) {
+    Object.defineProperty(this, "$__events__", {
+      value: /* @__PURE__ */ Object.create(null),
+      configurable: false,
+      enumerable: false,
+      writable: false
     });
-  },
-  ...param
+  }
+  if (!this.$__events__[event])
+    this.$__events__[event] = /* @__PURE__ */ new Set();
+  this.$__events__[event].add(callback);
 };
-var $off = {
-  value: function(event, callback) {
-    if (typeof callback === "function") {
-      this.removeEventListener(event, callback, false);
-      this.$__events__?.[event]?.delete(callback);
-    } else if (this.$__events__?.[event]) {
-      this.$__events__[event].forEach((cb) => {
-        this.removeEventListener(event, cb, false);
-      });
-      this.$__events__[event].clear();
-    }
-  },
-  ...param
+var $once = function(event, callback) {
+  this.addEventListener(event, callback, {
+    capture: false,
+    once: true
+  });
 };
-var $click = {
-  value: function(callback) {
-    if (typeof callback === "function") {
-      this.$on("click", callback, false);
-    } else {
-      this.click();
-    }
-  },
-  ...param
+var $off = function(event, callback) {
+  if (typeof callback === "function") {
+    this.removeEventListener(event, callback, false);
+    this.$__events__?.[event]?.delete(callback);
+  } else if (this.$__events__?.[event]) {
+    this.$__events__[event].forEach((cb) => {
+      this.removeEventListener(event, cb, false);
+    });
+    this.$__events__[event].clear();
+  }
 };
-var $contextmenu = {
-  value: function(callback) {
-    if (typeof callback === "function") {
-      this.$on("contextmenu", callback, false);
-    } else {
-      this.dispatchEvent("contextmenu");
-    }
-  },
-  ...param
+var $click = function(callback) {
+  if (typeof callback === "function") {
+    this.$on("click", callback, false);
+  } else {
+    this.click();
+  }
+};
+var $contextmenu = function(callback) {
+  if (typeof callback === "function") {
+    this.$on("contextmenu", callback, false);
+  } else {
+    this.dispatchEvent("contextmenu");
+  }
 };
 
 // lib/select.js
 var self = {
-  $select: {
-    value: function(el) {
-      return select(el, this);
-    },
-    ...param
+  $select: function(el) {
+    return select(el, this);
   },
-  $selectAll: {
-    value: function(el) {
-      return selectAll(el, this);
-    },
-    ...param
+  $selectAll: function(el) {
+    return selectAll(el, this);
   },
-  $parent: {
-    value: parent,
-    ...param
-  },
-  $prev: {
-    value: prev,
-    ...param
-  },
-  $next: {
-    value: next,
-    ...param
-  }
+  $parent: parent,
+  $prev: prev,
+  $next: next
+};
+var param = {
+  configurable: false,
+  enumerable: false,
+  writable: false
 };
 var properties = Object.assign({}, helper_exports, fx_exports, self);
+for (const k in properties) {
+  if (Object.hasOwn(properties, k))
+    properties[k] = { value: properties[k], ...param };
+}
 function define(el) {
   if (el && el instanceof HTMLElement) {
     Object.defineProperties(el, properties);
@@ -286,10 +219,10 @@ function parent(el = null) {
   return define(el ? this.closest(el) : this.parentNode);
 }
 function prev() {
-  return define(this.previousElementSibling);
+  return define(this.previousElementSibling ?? this.parentElement?.lastElementChild);
 }
 function next() {
-  return define(this.nextElementSibling);
+  return define(this.nextElementSibling ?? this.parentElement?.firstElementChild);
 }
 
 // lib/index.js
